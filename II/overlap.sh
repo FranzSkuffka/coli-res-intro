b60689a5a7655e950cc9118faa0e4fe3d80116d6
Jan Wirth <jan.wirth@cinteo.com>
2017-10-16 18:32:58 +0200

    Complete II

--- II/overlap.sh
+++ II/overlap.sh
@@ -1,3 +1,6 @@
 echo 'Starting overlap'
-echo $1 $2
-cat $1/* $2/*
+TEMP1=`mktemp`
+TEMP2=`mktemp`
+cat $1/* | gawk '{print tolower($0) }' | tr '[ .,/\-]' '\n' | sort > $TEMP1
+cat $2/* | gawk '{print tolower($0) }' | tr '[ .,/\-]' '\n' | sort > $TEMP2
+comm -1 -2 $TEMP1 $TEMP2 | uniq
